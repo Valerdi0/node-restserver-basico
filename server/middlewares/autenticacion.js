86c60cec28a9646d43c2934ec24755f9146e96cd
valerdi <dani_9_valerdi@hotmail.com>
2021-01-20 21:11:40 -0600

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -39,12 +39,31 @@ const VerificaAdmin_Role = (req, res, next) => {
             }
         });
     }
-
-
 };
 
+//====================
+// Verifica token para imagen
+//====================
+let VerificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
 
 module.exports = {
     VerificaToken,
-    VerificaAdmin_Role
+    VerificaAdmin_Role,
+    VerificaTokenImg
 }
